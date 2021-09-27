c66341f4bfc5441638c8e55ad2531b505eeb77b1
Eugen Illinskih <eilinskih92@gmail.com>
2021-09-27 09:19:39 +0300

    commit 1

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import './../App.css'
+import i from './../App.module.css'
 import MenuRGB from "./MenuRGB";
 import MenuSelect from "./MenuSelect";
 import { ReactComponent as DropDownLogo } from "./../drop-down-arrow.svg"
@@ -8,9 +8,7 @@ function Input() {
     let [color, setColor] = useState('#0000FF');
     let [isRgbActive, setIsRgbActive] = useState(false);
     let [isSelectActive, setIsSelectActive] = useState(false);
-    let background = {
-        backgroundColor: color
-    };
+    let [background, setBackground] = useState({backgroundColor: color});
 
     const onSelectClick = () => {
         setIsSelectActive(true)
@@ -21,28 +19,29 @@ function Input() {
     };
 
     const onInputChange = (e) => {
-        setColor(e.target.value)
+        setColor(e.target.value);
+        setBackground({backgroundColor: color})
     }
 
     const onColorChange = (newColor) => {
-        setColor(newColor)
+        setBackground({backgroundColor: newColor})
     };
 
     return (
         <>
-            <div className="input-container">
-                <div className="input">
-                    <input type="text" value={color} placeholder="#0000FF" onChange={onInputChange} />
+            <div className={i.inputContainer}>
+                <div className={i.input}>
+                    <input className={i.inputText} type="text" value={color} placeholder="#0000FF" onChange={onInputChange} />
                 </div>
-                <div className="color-container">
-                    <div style={background} className="color-div" onClick={onColorClick}></div>
+                <div className={i.colorContainer}>
+                    <div style={background} className={i.colorDiv} onClick={onColorClick}></div>
                 </div>
-                <div className="select-div" onClick={onSelectClick}>
+                <div className={i.selectDiv} onClick={onSelectClick}>
                     <DropDownLogo />
                 </div>
             </div>
 
-            {isRgbActive ? <MenuRGB onColorChange={onColorChange} /> : null}
+            {isRgbActive ? <MenuRGB color={color} onColorChange={onColorChange} /> : null}
             {isSelectActive ? <MenuSelect onColorChange={onColorChange} /> : null}
         </>
     )
--- src/components/MenuRGB.jsx
+++ src/components/MenuRGB.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import r from './MenuRGB.module.css';
 
 function MenuRGB(props) {
+    let redRef = React.createRef()
     return (
-       <>
-       </>
+        <>
+            <div className={r.container} >
+                <div>
+                    <label htmlFor="red">R</label>
+                    <input className={r.red} type="range" id="red" name="red"
+                        min="0" max="255" />
+                </div>
+                <div>
+                    <label htmlFor="green">G</label>
+                    <input className={r.green} type="range" id="green" name="green"
+                        min="0" max="255" />
+                </div>
+                <div>
+                    <label htmlFor="blue">B</label>
+                    <input className={r.blue} type="range" id="blue" name="blue"
+                        min="0" max="255" />
+                </div>
+                <div className={r.buttons}>
+                    <button>Cancel</button>
+                    <button>Save</button>
+                </div>
+
+            </div>
+        </>
     )
 }
 
