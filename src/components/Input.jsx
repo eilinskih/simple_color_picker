6c3ffd33cba0276864a81274848b9cd38c03080d
Evgeniy <eilinskih92@gmail.com>
2021-09-27 15:16:11 +0300

    commit for rewiew

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,48 +1,49 @@
 import React, { useState } from "react";
-import i from './../App.module.css'
+import i from './Input.module.css'
 import MenuRGB from "./MenuRGB";
 import MenuSelect from "./MenuSelect";
-import { ReactComponent as DropDownLogo } from "./../drop-down-arrow.svg"
 
 function Input() {
-    let [color, setColor] = useState('#0000FF');
+    let [color, setColor] = useState('#0000ff');
     let [isRgbActive, setIsRgbActive] = useState(false);
     let [isSelectActive, setIsSelectActive] = useState(false);
-    let [background, setBackground] = useState({backgroundColor: color});
+    let [background, setBackground] = useState({ backgroundColor: color });
 
     const onSelectClick = () => {
-        setIsSelectActive(true)
+        setIsSelectActive(isSelectActive ? false : true)
+        setIsRgbActive(false)
     };
 
     const onColorClick = () => {
-        setIsRgbActive(true)
+        setIsRgbActive(isRgbActive ? false : true)
+        setIsSelectActive(false)
     };
 
     const onInputChange = (e) => {
         setColor(e.target.value);
-        setBackground({backgroundColor: color})
+        setBackground({ backgroundColor: color })
     }
 
     const onColorChange = (newColor) => {
-        setBackground({backgroundColor: newColor})
+        setBackground({ backgroundColor: newColor })
     };
 
     return (
         <>
             <div className={i.inputContainer}>
                 <div className={i.input}>
-                    <input className={i.inputText} type="text" value={color} placeholder="#0000FF" onChange={onInputChange} />
+                    <input className={i.inputText} type="text" value={color} placeholder="#0000ff" onChange={onInputChange} />
                 </div>
                 <div className={i.colorContainer}>
                     <div style={background} className={i.colorDiv} onClick={onColorClick}></div>
                 </div>
                 <div className={i.selectDiv} onClick={onSelectClick}>
-                    <DropDownLogo />
+                    <div className={isSelectActive ? i.triangleBottomAct : i.triangleBottom} ></div>
                 </div>
             </div>
 
-            {isRgbActive ? <MenuRGB color={color} onColorChange={onColorChange} /> : null}
-            {isSelectActive ? <MenuSelect onColorChange={onColorChange} /> : null}
+            {isRgbActive ? <MenuRGB setColor={setColor} color={color} setActive={setIsRgbActive} onColorChange={onColorChange} /> : null}
+            {isSelectActive ? <MenuSelect onColorChange={onColorChange} setColor={setColor} setActive={setIsSelectActive} /> : null}
         </>
     )
 }
